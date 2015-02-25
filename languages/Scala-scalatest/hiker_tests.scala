f4868e26ad4e7d937c0566f529719a67fa9725a8
John St. John <johnthesaintjohn@gmail.com>
2015-02-25 22:18:11 +0000

    scalapuzzle.com specific changes

--- languages/Scala-scalatest/hiker_tests.scala
+++ languages/Scala-scalatest/hiker_tests.scala
@@ -5,10 +5,7 @@ import Hiker._
 class HikerSuite extends FunSuite {
   
   test("the answer to life the universe and everything") {
-    assert(answer() == 42)
-  }
-  test("Another example test") {
-    assert(answer() != 54)
+    assert(answer == 42)
   }
 
 }
